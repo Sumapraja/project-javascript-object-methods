3361ba0d762170e6a477294d4bea3f80ded80e78
Sumapraja <irfansumapraja@gmail.com>
2019-07-01 12:06:56 +0700

    adding some stuffs

--- index.js
+++ index.js
@@ -27,4 +27,44 @@ const bag = {
 console.log(bag.apple) 
 console.log(bag.banana);
 
+const makeUser = (name, age) => {
+  return {
+    name,
+    age
+  }
+}
 
+const user = makeUser('PelleK', 32)
+console.log(user);
+
+const clonedUser = {}
+
+for (let key in user){
+    clonedUser[key]=user[key]
+}
+
+clonedUser.name= 'RaonK'
+
+console.log(user);
+console.log(clonedUser);
+
+const newuser = {
+    name: 'John',
+    sizes: {
+      height: 182,
+      width: 50
+    }
+  }
+  
+  const newclonedUser = Object.assign({}, newuser)
+  
+  console.log(newuser.sizes === newclonedUser.sizes)
+
+  newuser.sizes.width++ 
+  console.log(newuser) 
+  console.log(newclonedUser) 
+  
+  Object.assign(newclonedUser, {name: 'Jay', sizes:{ height: 179}})
+
+  console.log(newclonedUser);
+  
